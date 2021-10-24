25acd307696819bec25181a29df38f86be4e497e
Yves Ruosch <yves@ruosch.me>
2021-10-24 18:02:01 +0200

    azure test

---
+++ src/main/java/me/ruosch/zinsen/ZinsenApplication.java
@@ -0,0 +1,13 @@
+package me.ruosch.zinsen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZinsenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZinsenApplication.class, args);
+	}
+
+}
