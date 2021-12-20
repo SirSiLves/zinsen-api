c65fdb76404a116cc2191f489d33a0a347945bcf
Yves Ruosch <yves@ruosch.me>
2021-12-20 18:04:06 +0100

    Application Insights

--- src/main/java/me/ruosch/zinsen/core/TelemetryClientConfig.java
+++ src/main/java/me/ruosch/zinsen/core/TelemetryClientConfig.java
@@ -9,6 +9,9 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class TelemetryClientConfig {
 
+    @Value("${azure.application-insights.name}")
+    private String roleName;
+
     @Value("${azure.application-insights.instrumentation-key}")
     private String instrumentationKey;
 
@@ -21,9 +24,12 @@ public class TelemetryClientConfig {
     public TelemetryConfiguration telemetryConfiguration() {
         TelemetryConfiguration telemetryConfiguration = TelemetryConfiguration.getActive();
         telemetryConfiguration.setInstrumentationKey(instrumentationKey);
+        telemetryConfiguration.setConnectionString("InstrumentationKey=37402c98-17e2-4f2c-a6ac-0355e698ff34;IngestionEndpoint=https://westeurope-5.in.applicationinsights.azure.com/");
+        telemetryConfiguration.setRoleName(roleName);
 
         return telemetryConfiguration;
     }
+
 
 
 }
